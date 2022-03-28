88e974a24ab83beeb563dcceb7e9946171798c53
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-03-28 15:39:06 -0400

    Added icons.

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -5,6 +5,12 @@ import apple from '../../Img/apple.png'
 import amazon from '../../Img/amazon.png'
 import cr from '../../Img/cr.png'
 import disney from '../../Img/disney.png'
+import hbo from '../../Img/hbo.png'
+import hulu from '../../Img/hulu.png'
+import netflix from '../../Img/netflix.png'
+import paramount from '../../Img/paramount.png'
+import peacock from '../../Img/peacock.png'
+import funimation from '../../Img/funimation.png'
 
 class Calculator extends Component {
     state = {
@@ -40,33 +46,33 @@ class Calculator extends Component {
 
                     <Row columns={1}>
                         <Col>
-                            <Image className="icon" src={apple} />
+                            <Image className="icon" src={funimation} />
                         </Col>
                         <Col>
-                            <Image className="icon" src={apple} />
+                            <Image className="icon" src={hbo} />
                         </Col>
                         <Col>
-                            <Image className="icon" src={apple} />
+                            <Image className="icon" src={hulu} />
                         </Col>
                         <Col>
-                            <Image className="icon" src={apple} />
+                            <Image className="icon" src={netflix} />
                         </Col>
                     </Row>
                     <br></br>
 
                     <Row columns={1}>
                         <Col>
+                            <Image className="icon" src={paramount} />
+                        </Col>
+                        <Col>
+                            <Image className="icon" src={peacock} />
+                        </Col>
+                        {/* <Col>
                             <Image className="icon" src={apple} />
                         </Col>
                         <Col>
                             <Image className="icon" src={apple} />
-                        </Col>
-                        <Col>
-                            <Image className="icon" src={apple} />
-                        </Col>
-                        <Col>
-                            <Image className="icon" src={apple} />
-                        </Col>
+                        </Col> */}
                     </Row>
                     <br></br>
 
